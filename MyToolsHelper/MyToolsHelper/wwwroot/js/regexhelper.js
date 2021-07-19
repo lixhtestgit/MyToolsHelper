f332c368d9e762ae85861eb07fa7d6e9a70b7f3b
lixianghong <lixianghong@meshop.net>
2021-07-19 14:34:53 +0800

    添加url获取正则表达式

---
+++ MyToolsHelper/MyToolsHelper/wwwroot/js/regexhelper.js
@@ -0,0 +1,10 @@
+﻿//获取url参数值
+function getQueryString(url, name) {
+    var reg = new RegExp("(?<=[?&]{1}" + name + "=)[^&]*");
+    var r = url.match(reg);
+    if (r != null) {
+        return unescape(r[0]);
+    } else {
+        return null;
+    }
+}
